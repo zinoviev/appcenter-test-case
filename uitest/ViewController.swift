4010dc7410172db514dbb13e2af30798b2b2eb8d
Igor Zinovev <igor.v.zinovev@gmail.com>
2021-02-01 18:19:09 +0300

    Initial Commit

---
+++ uitest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uitest
+//
+//  Created by Igor Zinovev on 01.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
